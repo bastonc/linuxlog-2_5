3f8e973348c4d6baa35e18ea990f11b493c1edd5
bastonc <bastonsv@gmail.com>
2023-09-01 23:42:22 +0300

    fix create DB

--- main.py
+++ main.py
@@ -3461,6 +3461,7 @@ class LogForm(QMainWindow):
                 'STATION_CALLSIGN': call}
 
             logWindow.addRecord(self.recordObject)
+            # Check current spot
             if self.current_spot is not None and call == self.current_spot['call']:
                 self.current_spot['complete'] = 1
                 self.spot_index_by_band[self.get_current_band()] -= 1
